e930f35fa74f57ec06d2094070b13ed501ee8d58
Michael Ndon <manj19.mjn@gmail.com>
2020-11-14 22:08:15 +0100

    Looped through csv and ran subprocess command on each line

--- PythonDemo/demo.py
+++ PythonDemo/demo.py
@@ -1,1 +1,11 @@
 import csv
+import subprocess
+
+with open('userlist.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    line_count = 0
+    for line in csv_reader:
+        if line_count != 0:
+            fullname, fname, lname, sam_name, upn, password = line
+            subprocess.run(f'dsadd user "cn={sam_name},ou=NIITUSERS,dc=niit,dc=com" -samid {sam_name} -upn {upn} -pwd {password} -fn {fname} -ln {lname} -disabled no -mustchpwd yes ')
+
